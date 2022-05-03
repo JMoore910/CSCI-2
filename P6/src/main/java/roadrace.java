f17c295424540a74b13abe50c7c31329de2a81fc
Jeanne Moore <mooresea910@gmail.com>
2022-05-03 15:27:45 -0400

    Added more comments, refactored P6 methods

--- P6/src/main/java/roadrace.java
+++ P6/src/main/java/roadrace.java
@@ -4,15 +4,20 @@
  */
 
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class roadrace {
     //  Dynamic Programming Globals needed:
-    //  A 1D array storing d^2 + 5 for every d
-    //  A 2D array travel time on each lane
-    private static int n, m;
-    private static int[] changeLane, dist;
+    //  Two int vars to store the num of segments, and the num of lanes per segment
+    //  A 1D array storing lane change times based on lane difference
+    //  A 1D array storing the distances of each segment
+    //  A 2D array storing the average speeds found in each lane
+    //  A 2D array storing the dynamic programming memoization of the shortest times to each lane
+
+    private static int n;
+    private static int m;
+    private static int[] changeLane;
+    private static int[] dist;
     private static int[][] lanes;
     private static double[][] dp;
 
@@ -40,48 +45,72 @@ public class roadrace {
             }
             //  If not looking at the first segment, the program must go through the previous lanes time and find the
             //  least time adding on the time to change lanes from the current one
-            else {
-                for (int j = 1; j <= m; j++) {
-                    double lowest = Integer.MAX_VALUE;
-                    for (int k = 1; k <= m; k++) {
-                        int change = Math.abs(k-j);
-                        if (change == 0){
-                            //  No lane change, no extra time
-                            lowest = Math.min(lowest, dp[i-1][k] + 5);
-                        }
-                        else {
-                            //  Lane change, so add change time
-                            lowest = Math.min(lowest, dp[i-1][k] + changeLane[change]);
-                        }
-                    }
 
-                    //  We now have lowest, assign it to dp[i][j] and add the time it takes to traverse this segment
-                    dp[i][j] = (lowest + ((dist[i]*3600)/(1.0 * (lanes[i][j] * 1000))));
-                }
+
+            else {
+                //  Call dynamic programming method
+                dynamicRace(i);
             }
+
+
+
         }
 
+        //  Set lowest to be a max value, find lowest value representing shortest path
         double lowest = Double.MAX_VALUE;
         for (int j = 1; j <= m; j++) {
-            if (lowest > dp[n-1][j]) lowest = dp[n-1][j];
+            //  If lowest is larger than current dp element, set dp[n-1][j] to be lowest
+            if (lowest > dp[n-1][j])
+                lowest = dp[n-1][j];
         }
 
+        //  Print out shortest path
+        printPath();
+
+        //  Print out shortest distance
         System.out.printf("%.2f ",lowest);
     }
 
 
+    private static void dynamicRace(int i){
+        //  Go through each element in dp calculating shortest time to reach and traverse that lane
+        for (int j = 1; j <= m; j++) {
+            double lowest = Integer.MAX_VALUE;
+            for (int k = 1; k <= m; k++) {
+                int change = Math.abs(k-j);
+                if (change == 0){
+                    //  No lane change
+                    lowest = Math.min(lowest, dp[i-1][k] + 5);
+                }
+                else {
+                    //  Lane change, so add change time
+                    lowest = Math.min(lowest, dp[i-1][k] + changeLane[change]);
+                }
+            }
+
+            //  We now have lowest, assign it to dp[i][j] and add the time it takes to traverse this segment
+            dp[i][j] = (lowest + ((dist[i]*3600)/(1.0 * (lanes[i][j] * 1000))));
+        }
+
+
+    }
+
+
     private static void printPath() {
+        //  Initialize a lowest variable and index
         int lowestIndex;
         double lowest;
         int[] path = new int[n];
 
-        //  Start at the end of dp
+        //  Start at the end of dp and move in reverse to build back the path
         for (int i = n-1; i >= 0; i--) {
+            //  For each segment start, set the lowest to be the max value
             lowest = Double.MAX_VALUE;
             lowestIndex = 0;
             if (i == n-1) {
-                //  find lowest time in lane
+                //  find lowest time in lane in last legment
                 for (int j = 1; j <= m; j++) {
+                    //  If the lowest is larger than the current value, we have found the new lowest
                     if (lowest > dp[i][j]) {
                         lowestIndex = j;
                         lowest = dp[i][j];
@@ -94,6 +123,7 @@ public class roadrace {
                 int change;
                 for (int j = 1; j <= m; j++) {
                     change = Math.abs(j - last);
+                    //  Use change time to determine whether this is the new lowest time value or not
                     if (lowest > (dp[i][j] + changeLane[change])) {
                         lowestIndex = j;
                         lowest = dp[i][j];
@@ -116,12 +146,13 @@ public class roadrace {
 
     private static void readInput() {
 
-        //  INPUT:
+        //  Set scanner to take in input
         Scanner input = new Scanner(System.in);
 
         String line = input.nextLine();
         String[] nums = line.split(" ");
 
+        //  Read in num of segments n and num of lanes per segment m
         n = Integer.parseInt(nums[0]);
         m = Integer.parseInt(nums[1]);
 
